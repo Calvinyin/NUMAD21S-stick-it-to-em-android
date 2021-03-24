cdcddededf1fc505a8b37717561bab540d7212db
calvinyin <calvinyin1991@gmail.com>
2021-03-23 22:36:30 -0700

    Login activity added

---
+++ app/src/main/java/edu/neu/madcourse/numad21s_stick_it_to_em_android/LoginActivity.java
@@ -0,0 +1,14 @@
+package edu.neu.madcourse.numad21s_stick_it_to_em_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
