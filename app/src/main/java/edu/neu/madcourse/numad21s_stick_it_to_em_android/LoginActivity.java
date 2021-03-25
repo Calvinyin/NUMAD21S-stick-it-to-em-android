ee758395f2cc4b1d085856066cddf1e48abfbccc
calvinyin <calvinyin1991@gmail.com>
2021-03-25 14:19:29 -0700

    login page fixed

--- app/src/main/java/edu/neu/madcourse/numad21s_stick_it_to_em_android/LoginActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21s_stick_it_to_em_android/LoginActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,20 +22,20 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
         usernameEditText = (EditText) findViewById(R.id.editText_username);
-        loginButton = findViewById(R.id.button_login);
-        username = usernameEditText.getText().toString();
+    }
 
-        // navigate to the main activity when click login
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                intent.putExtra("username", username);
-                startActivity(intent);
-            }
-        });
+    public void login(View view) {
+        loginButton = (Button)findViewById(R.id.button_login);
+        username = usernameEditText.getText().toString();
+        Log.d(TAG, "username = " + username);
+
+        if (username.equals(null) || username.length() == 0) {
+            Toast.makeText(LoginActivity.this, "Please input a username!", Toast.LENGTH_LONG).show();
+        } else {
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            intent.putExtra("username", username);
+            startActivity(intent);
+        }
     }
 }
